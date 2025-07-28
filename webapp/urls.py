fbd5d4d35412f53e942e3aecffed1c1e1890a2b2
Grajesh Chandra <grajesh.c@affinidi.com>
2025-07-28 14:39:37 +0530

    update IDV

--- webapp/urls.py
+++ webapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("iota/", views.iota, name="iota"),
     path("revoke/", views.revoke, name="revoke"),
     path("verify/", views.verify, name="verify"),
+    path("idv/", views.idv, name="idv"),
     path("api/hello/", api.api_hello, name="api_hello"),
     path("api/issue-credential", api.issue_credential, name="issue_credential"),
     path("api/fetch-credential", api.fetch_credential, name="fetch_credential"),
--- webapp/views.py
+++ webapp/views.py
@@ -11,6 +11,21 @@ def home(request):
 def cis(request):
     return render(request, "webapp/cis.html")
 
+def idv(request):
+    config_id_idv = os.environ.get("IOTA_CONFIG_ID_IDV", "")
+    avvanz_query_id_idv = os.environ.get("IOTA_AVVANZ_CREDENTIAL_QUERY_IDV", "")
+    response_code = request.GET.get("response_code", "")
+
+    return render(
+        request,
+        "webapp/idv.html",
+        {
+            "config_id_idv": config_id_idv,
+            "avvanz_query_id_idv": avvanz_query_id_idv,
+            "response_code": response_code,
+        },
+    )
+
 
 def iota(request):
     config_id = os.environ.get("IOTA_CONFIG_ID", "")
